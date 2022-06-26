d773e62d2045e61cf6ac76e14316f01d386613cf
balabalu123 <balaaccudr@gmail.com>
2022-06-26 12:07:48 +0530

    Prakash is pulling the code

--- src/org/selenium/ContainsusingAttributes.java
+++ src/org/selenium/ContainsusingAttributes.java
@@ -6,6 +6,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ContainsusingAttributes {
+	
+	public void Attribute() {
+		System.out.println("Attribute");
+
+	}
 
 	
 	public static void main(String[] args) {
